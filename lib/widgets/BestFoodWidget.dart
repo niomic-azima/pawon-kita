d6ca1f902b98051c451f95bde88ac13b3eb6b8eb
azima-popo <anggagardiawan@gmail.com>
2021-02-04 04:50:03 -0800

    Pawon Kita

---
+++ lib/widgets/BestFoodWidget.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class BestFoodWidget extends StatefulWidget {
+  @override
+  _BestFoodWidgetState createState() => _BestFoodWidgetState();
+}
+
+class _BestFoodWidgetState extends State<BestFoodWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      width: double.infinity,
+      child: Column(
+        children: <Widget>[
+          BestFoodTitle(),
+          Expanded(
+            child: BestFoodList(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BestFoodTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "Makanan Favorit",
+            style: TextStyle(
+                fontSize: 20,
+                color: Color(0xFF3a3a3b),
+                fontWeight: FontWeight.w300),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BestFoodTiles extends StatelessWidget {
+  String name;
+  String imageUrl;
+  String rating;
+  String numberOfRating;
+  String price;
+  String slug;
+
+  BestFoodTiles(
+      {Key key,
+      @required this.name,
+      @required this.imageUrl,
+      @required this.rating,
+      @required this.numberOfRating,
+      @required this.price,
+      @required this.slug})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 5, top: 5, bottom: 5),
+            decoration: BoxDecoration(boxShadow: [
+              /* BoxShadow(
+                color: Color(0xFFfae3e2),
+                blurRadius: 15.0,
+                offset: Offset(0, 0.75),
+              ),*/
+            ]),
+            child: Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Image.asset(
+                'assets/images/popular_foods/' + imageUrl + ".jpeg",
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 1,
+              margin: EdgeInsets.all(5),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BestFoodList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        BestFoodTiles(
+            name: "Bakso",
+            imageUrl: "bakso",
+            rating: '4.9',
+            numberOfRating: '200',
+            price: '15.06',
+            slug: "fried_egg"),
+        BestFoodTiles(
+            name: "Mie Ayam",
+            imageUrl: "mie_ayam",
+            rating: "4.5",
+            numberOfRating: "100",
+            price: "17.03",
+            slug: ""),
+        BestFoodTiles(
+            name: "Nasi Goreng",
+            imageUrl: "nasi_goreng",
+            rating: "4.0",
+            numberOfRating: "50",
+            price: "11.00",
+            slug: ""),
+        BestFoodTiles(
+            name: "Sosis",
+            imageUrl: "sosis",
+            rating: "4.00",
+            numberOfRating: "100",
+            price: "11.10",
+            slug: ""),
+        BestFoodTiles(
+            name: "Soto",
+            imageUrl: "soto",
+            rating: "4.6",
+            numberOfRating: "150",
+            price: "12.00",
+            slug: ""),
+        BestFoodTiles(
+            name: "Sate",
+            imageUrl: "sate",
+            rating: "4.00",
+            numberOfRating: "100",
+            price: "11.10",
+            slug: ""),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/TopMenus.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class TopMenus extends StatefulWidget {
+  @override
+  _TopMenusState createState() => _TopMenusState();
+}
+
+class _TopMenusState extends State<TopMenus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          TopMenuTiles(name: "Dawet", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Lupis", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Bolu", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Kue", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Kripik", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Sosis", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Jajanan Seribu", imageUrl: "", slug: ""),
+          TopMenuTiles(name: "Lumpia", imageUrl: "", slug: ""),
+        ],
+      ),
+    );
+  }
+}
+
+class TopMenuTiles extends StatelessWidget {
+  String name;
+  String imageUrl;
+  String slug;
+
+  TopMenuTiles(
+      {Key key,
+      @required this.name,
+      @required this.imageUrl,
+      @required this.slug})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 5, top: 5, bottom: 5),
+            decoration: new BoxDecoration(boxShadow: [
+              new BoxShadow(
+                color: Color(0xFFfae3e2),
+                blurRadius: 25.0,
+                offset: Offset(0.0, 0.75),
+              ),
+            ]),
+            child: Card(
+                color: Colors.white,
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(3.0),
+                  ),
+                ),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Center(
+                      child: Image.asset(
+                    'assets/images/topmenu/' + imageUrl + ".png",
+                    width: 24,
+                    height: 24,
+                  )),
+                )),
+          ),
+          Text(name,
+              style: TextStyle(
+                  color: Color(0xFF6e6e71),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400)),
+        ],
+      ),
+    );
+  }
+}
